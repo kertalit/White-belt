6c5db59ff7665d9f7d471e3afd43d31040de809f
Alexey Abubyakirov <kertalit@users.noreply.github.com>
2024-03-09 20:24:52 +0300

    creating a reverse vector through a constructor and a copy

--- Week_2/SequenceReversal2.cpp
+++ Week_2/SequenceReversal2.cpp
@@ -5,25 +5,22 @@ std::vector<int> SequenceReversal(const std::vector<int>& v);
 
 int main()
 {
-  std::vector<int> numbers = { 1, 5, 3, 4, 2 };
-  auto reverse = SequenceReversal(numbers);
+    std::vector<int> numbers = { 1, 5, 3, 4, 2 };
 
-  for (auto n : reverse)
-  {
-    std::cout << n << std::endl;
-  }
+    std::vector<int> reverse(numbers.rbegin(), numbers.rend());
+
+    for (auto n : reverse)
+    {
+        std::cout << n << std::endl;
+    }
 }
 
 
 std::vector<int> SequenceReversal(const std::vector<int>& v)
 {
-  std::vector<int> reverse;
-  auto index = v.size() - 1;
+    std::vector<int> reverse(v.size());
 
-  for (int i = index; i >= 0; --i)
-  {
-    reverse.push_back(v[i]);
-  }
+    std::copy(v.rbegin(), v.rend(), reverse.begin());
 
-  return reverse;
+    return reverse;
 }
