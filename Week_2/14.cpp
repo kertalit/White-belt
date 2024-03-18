0ffb5b8214d9bec4e449ede26dd468b251f4edf6
Sergey Kashtanov <skashtanov@ksalternative.com>
2024-03-18 18:03:13 +0300

    Update 14.cpp

--- Week_2/14.cpp
+++ Week_2/14.cpp
@@ -17,13 +17,27 @@ int main()
         std::cin >> numberRoutes;
 
         std::vector<std::string> stops;
+        stops.reserve(numberRoutes);
 
         for (int i = 0; i < numberRoutes; ++i)
         {
             std::string temp;
             std::cin >> temp;
-            stops.push_back(temp);
+            stops.push_back(move(temp));
         }
+
+#if USE_OPTIMIZED
+        size_t currentSize = routes.size();
+        auto [it, inserted] = routes.insert({ std::move(stops), currentSize + 1 });
+        if (inserted == false) 
+        {
+            std::cout << "Already exists for " << it->second << '\n';
+        } 
+        else 
+        {
+            std::cout << "New bus " << it->second << '\n';
+        }
+#else   
         if (routes.count(stops))
         {
             std::cout << "Already exists for " << routes[stops] << '\n';
@@ -32,9 +46,9 @@ int main()
         {
             int size = routes.size();
             routes[stops] = ++size;
-
             std::cout << "New bus " << size << '\n';
         }
+#endif
     }
 
     return 0;
